98bba53426082147167974422235e06a2b713f06
Joseph Grigg <joegrigg64@gmail.com>
2021-12-08 01:23:23 -0500

    maybe some progress

--- p08_aoc.py
+++ p08_aoc.py
@@ -31,20 +31,29 @@ def part1():
 def part2():
     '''day 8 part 2'''
 
+    digit_sum = 0
     original_digits = {'abcefg': '0', 'cf': '1', 'acdeg': '2', 'acdfg': '3', 'bcdf': '4', 'abdfg': '5', 'abdefg': '6', 'acf': '7', 'abcdefg': '8', 'abcdfg': '9' }
     segment_mapper = {'a': '', 'b': '', 'c': '', 'd': '', 'e': '', 'f': '', 'g': ''}
 
+    def decoder(corrupt_digits):
+        '''maybe do some decoding to solve this thing'''
+        d_map = {}
+
+        return d_map
 
     with open(infile_path, 'r') as f:
         lines = f.readlines()
 
         for line in lines:
             output_num = ''
-            output = line.split(' | ')[1].strip()
-            digits = output.split(' ')
-            for digit in digits:
-                ''
-            print(output_num)
+            (corrupt_digits, output) = line.strip().split(' | ')
+            corrupt_digits = corrupt_digits.strip().split(' ')
+            out_digits = output.strip().split(' ')
+            d_map = decoder(corrupt_digits)
+            for digit in out_digits:
+                if ''.join(sorted(digit)) in d_map:
+                    output_num += d_map[digit]
+            digit_sum += int(output_num)
 
     return 'not done'
 
